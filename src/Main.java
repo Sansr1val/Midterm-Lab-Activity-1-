7b30c6c1ee9858216f9bed37a401038884c26437
John Jomar <dimaunahanjohnjomar@gmail.com>
2023-03-18 13:46:54 +0800

    Add Main Menu format

--- src/Main.java
+++ src/Main.java
@@ -26,7 +26,7 @@ public class Main {
 				System.out.println("Only positive integer inputs are allowed! Try again.\n");
 			}
 		}
-		
+
 		/*
 		 * Places values inside the list depending on the 
 		 * size input of the user.
@@ -44,12 +44,54 @@ public class Main {
 				}
 			}
 		}
-		
+
 		//Sorts the list before operations can be done in the menu
 		System.out.println("\nSorting the List...");
 		list.bubbleSort();
 		list.display();
-		
-		
+
+
+		try {
+			while(true) {
+				System.out.println("\n-----Menu-----");
+				System.out.println("[1] Add Node");
+				System.out.println("[2] Create Skip List");
+				System.out.println("[3] Display List From Head");
+				System.out.println("[4] Display List From Tail");
+				System.out.println("[5] Display Skip List From Head");
+				System.out.println("[6] Display Skip List From Tail");
+				System.out.println("[7] Search From Head");
+				System.out.println("[8] Search From Tail");
+				System.out.println("[9] Exit");
+				System.out.println("Select an operation: ");
+				int choice = reader.readLine().charAt(0);
+				if(choice < 1 || choice > 9){
+					System.out.println("Operation "+ choice+" does not exist! Try again.");
+				}else {
+					switch(choice) {
+					case 1://add
+						break;
+					case 2://create skip list
+						break;
+					case 3://display list from head
+						break;
+					case 4://display list from tail
+						break;
+					case 5://display skip list from head
+						break;
+					case 6://display skip list from tail
+						break;
+					case 7://search from head
+						break;
+					case 8://search from tail
+						break;
+					case 9://exit
+						break;
+					}
+				}
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("Only integers are allowed as inputs! Try again.");
+		}
 	}
 }

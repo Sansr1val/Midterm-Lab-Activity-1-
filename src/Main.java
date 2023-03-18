387594a7081785125291283ee1111a905dd384cc
John Jomar <dimaunahanjohnjomar@gmail.com>
2023-03-18 12:48:48 +0800

    Added List size initialization

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	static int size;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//Testing lang
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		while(true) {
+			try {
+				System.out.print("Enter List size: ");
+				size = Integer.parseInt(reader.readLine());
+				if(size<=0) {
+					throw new NumberFormatException();
+				}else {
+					break;
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Only positive integer inputs are allowed! Try again.\n");
+			}
+		}
+		
+
 	}
-
 }

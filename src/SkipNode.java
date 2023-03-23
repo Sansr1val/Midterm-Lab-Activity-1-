9ebdc8b0fc6017bf59a7b027ed71722f6b3b2f60
John Emmanuel Sales <66854142+JE-Sales@users.noreply.github.com>
2023-03-23 14:18:13 +0800

    New Constructor with Bottom Parameter

--- src/SkipNode.java
+++ src/SkipNode.java
@@ -1,56 +1,64 @@
 
 public class SkipNode {
-	
+
 	private int data;
 	private SkipNode previous;
 	private SkipNode next;
 	private Node bottom;
-	
-	//constructor
+
+	// constructor
 	public SkipNode() {
 		this.data = 0;
 		this.next = null;
 		this.previous = null;
 		this.bottom = null;
 	}
+
 	public SkipNode(int data) {
 		this.data = data;
 		this.next = null;
 		this.previous = null;
 		this.bottom = null;
 	}
-	
-	//setters and getters.
+
+	public SkipNode(int data, Node bottom) {
+		this.data = data;
+		this.next = null;
+		this.previous = null;
+		this.bottom = bottom;
+	}
+
+	// setters and getters.
 	public int getData() {
 		return this.data;
 	}
-	
+
 	public void setData(int data) {
 		this.data = data;
 	}
-	
+
 	public SkipNode getNext() {
 		return this.next;
 	}
-	
+
 	public void setNext(SkipNode next) {
 		this.next = next;
 	}
-	
+
 	public SkipNode getPrevious() {
 		return this.previous;
 	}
-	
+
 	public void setPrevious(SkipNode previous) {
 		this.previous = previous;
 	}
-	
+
 	public Node getBottom() {
 		return this.bottom;
 	}
-	
+
 	public void setBottom(Node bottom) {
 		this.bottom = bottom;
 	}
-	
+
 }

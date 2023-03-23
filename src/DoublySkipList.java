2ca9f3634274894e639ce29b88b7f5db9513510f
DyamesGH <114891651+DyamesGH@users.noreply.github.com>
2023-03-23 15:57:13 +0800

    Its me again SearchFromTail *NOT DONE!!!

--- src/DoublySkipList.java
+++ src/DoublySkipList.java
@@ -4,7 +4,7 @@ public class DoublySkipList {
 	private SkipNode head;
 	private SkipNode tail;
 
-	//setters and getters
+	// setters and getters
 	public void setHead(SkipNode head) {
 		this.head = head;
 	}
@@ -21,27 +21,26 @@ public class DoublySkipList {
 		return this.tail;
 	}
 
-	public void add (SkipNode skipNode) {
-		if(head == null) {
+	public void add(SkipNode skipNode) {
+		if (head == null) {
 			head = skipNode;
 			tail = skipNode;
-		}else {
+		} else {
 			tail.setNext(skipNode);
 			skipNode.setPrevious(tail);
 			tail = skipNode;
 		}
 	}
 
-
 	public String display() {
 		SkipNode currentNode = head;
 		String str = "";
-		while(currentNode != null) {
-			str += String.valueOf(currentNode.getData()) +", ";
+		while (currentNode != null) {
+			str += String.valueOf(currentNode.getData()) + ", ";
 			currentNode = currentNode.getNext();
 		}
-		if(str !="") {
-			str = str.substring(0,str.length() -2);
+		if (str != "") {
+			str = str.substring(0, str.length() - 2);
 		}
 		return str;
 	}
@@ -49,27 +48,26 @@ public class DoublySkipList {
 	public String displayReverse() {
 		SkipNode currentNode = tail;
 		String str = "";
-		while(currentNode != null) {
-			str += String.valueOf(currentNode.getData()) +", ";
+		while (currentNode != null) {
+			str += String.valueOf(currentNode.getData()) + ", ";
 			currentNode = currentNode.getPrevious();
 		}
-		if(str !="") {
-			str = str.substring(0,str.length() -2);
+		if (str != "") {
+			str = str.substring(0, str.length() - 2);
 		}
 		return str;
 	}
-	
+
 	public String searchFromHead(int num) {
 		SkipNode tempSkipNode = this.head;
 		Node tempNode = new Node();
 		String res = "Skip List:";
 
-		
 		while (tempSkipNode != null && tempSkipNode != tail && tempSkipNode.getNext().getData() <= num) {
 			res += (" " + tempSkipNode.getData());
 			tempSkipNode = tempSkipNode.getNext();
 		}
-		
+
 		res += (" " + tempSkipNode.getData());
 
 		if (num == tempSkipNode.getData()) {
@@ -81,12 +79,55 @@ public class DoublySkipList {
 				res += " " + tempNode.getData();
 				tempNode = tempNode.getNext();
 			}
-			
 
 		}
 
-		res = tempNode != null && tempNode.getData() == num? res += " " + tempNode.getData() : "Number does not exist in the list.";
+		res = tempNode != null && tempNode.getData() == num ? res += " " + tempNode.getData()
+				: "Number does not exist in the list.";
 		return res;
-		
+
+	}
+
+	public String searchFromTail(int num) {
+		SkipNode tempSkipNode = this.tail;
+		Node tempNode = new Node();
+		String res = "Skip List:";
+
+		if (tempSkipNode.getData() == num)
+			return res + " " + tempSkipNode;
+
+		if (num > tempSkipNode.getData()) {
+			tempNode = tempSkipNode.getBottom();
+			res += "\nLinked List:";
+			while (tempNode != null && num != tempNode.getData()) {
+				res += " " + tempNode.getData();
+				tempNode = tempNode.getNext();
+			}
+		} else {
+
+			while (tempSkipNode != null && tempSkipNode != head && tempSkipNode.getPrevious().getData() >= num) {
+				res += (" " + tempSkipNode.getData());
+				tempSkipNode = tempSkipNode.getPrevious();
+			}
+
+			res += (" " + tempSkipNode.getData());
+
+			if (num == tempSkipNode.getData()) {
+				return res;
+			} else {
+				tempNode = tempSkipNode.getBottom();
+				res += "\nLinked List:";
+				while (tempNode != null && num != tempNode.getData()) {
+					res += " " + tempNode.getData();
+					tempNode = tempNode.getPrevious();
+				}
+
+			}
+
+		}
+
+		res = tempNode != null && tempNode.getData() == num ? res += " " + tempNode.getData()
+				: "Number does not exist in the list.";
+		return res;
 	}
 }

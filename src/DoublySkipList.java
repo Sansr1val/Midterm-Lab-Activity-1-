cb180e4c60ece42c216f63e1efb19840181a114c
DyamesGH <114891651+DyamesGH@users.noreply.github.com>
2023-03-23 15:39:08 +0800

    ITS ALL ME SEARCHFROMHEAD!!!-

--- src/DoublySkipList.java
+++ src/DoublySkipList.java
@@ -3,9 +3,8 @@ public class DoublySkipList {
 
 	private SkipNode head;
 	private SkipNode tail;
-	private Node bottom;
 
-	// setters and getters
+	//setters and getters
 	public void setHead(SkipNode head) {
 		this.head = head;
 	}
@@ -22,35 +21,27 @@ public class DoublySkipList {
 		return this.tail;
 	}
 
-	public void setBottom(Node bottom) {
-		this.bottom = bottom;
-	}
-
-	public Node getBottom() {
-		return this.bottom;
-	}
-
-	public void add(int data) {
-		SkipNode node = new SkipNode(data);
-		if (head == null) {
-			head = node;
-			tail = node;
-		} else {
-			tail.setNext(node);
-			node.setPrevious(tail);
-			tail = node;
+	public void add (SkipNode skipNode) {
+		if(head == null) {
+			head = skipNode;
+			tail = skipNode;
+		}else {
+			tail.setNext(skipNode);
+			skipNode.setPrevious(tail);
+			tail = skipNode;
 		}
 	}
+
 
 	public String display() {
 		SkipNode currentNode = head;
 		String str = "";
-		while (currentNode != null) {
-			str += String.valueOf(currentNode.getData()) + ", ";
+		while(currentNode != null) {
+			str += String.valueOf(currentNode.getData()) +", ";
 			currentNode = currentNode.getNext();
 		}
-		if (str != "") {
-			str = str.substring(0, str.length() - 2);
+		if(str !="") {
+			str = str.substring(0,str.length() -2);
 		}
 		return str;
 	}
@@ -58,41 +49,44 @@ public class DoublySkipList {
 	public String displayReverse() {
 		SkipNode currentNode = tail;
 		String str = "";
-		while (currentNode != null) {
-			str += String.valueOf(currentNode.getData()) + ", ";
+		while(currentNode != null) {
+			str += String.valueOf(currentNode.getData()) +", ";
 			currentNode = currentNode.getPrevious();
 		}
-		if (str != "") {
-			str = str.substring(0, str.length() - 2);
+		if(str !="") {
+			str = str.substring(0,str.length() -2);
 		}
 		return str;
 	}
-
-	public void searchFromHead(int num) {
-		SkipNode tempSkipNode = head;
+	
+	public String searchFromHead(int num) {
+		SkipNode tempSkipNode = this.head;
 		Node tempNode = new Node();
 		String res = "Skip List:";
 
-		while (num > tempSkipNode.getData() && tempSkipNode != null) {
+		
+		while (tempSkipNode != null && tempSkipNode != tail && tempSkipNode.getNext().getData() <= num) {
 			res += (" " + tempSkipNode.getData());
 			tempSkipNode = tempSkipNode.getNext();
 		}
+		
+		res += (" " + tempSkipNode.getData());
 
 		if (num == tempSkipNode.getData()) {
-			System.out.println(res + " " + tempSkipNode.getData());
+			return res;
 		} else {
 			tempNode = tempSkipNode.getBottom();
-			res += "/nLinked List:";
-			while (num < tempNode.getData() && tempNode != null) {
+			res += "\nLinked List:";
+			while (tempNode != null && num != tempNode.getData()) {
 				res += " " + tempNode.getData();
 				tempNode = tempNode.getNext();
 			}
-			res += " " + tempNode.getData();
+			
 
 		}
 
-		System.out.println(tempNode.getData() == num ? res : "Number does not exist in the list.");
-
+		res = tempNode != null && tempNode.getData() == num? res += " " + tempNode.getData() : "Number does not exist in the list.";
+		return res;
+		
 	}
-
 }
